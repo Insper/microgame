017a32e9d22e44a9b6b2daf568c5659164dfca36
Emil Freme <emil@1000fps.co>
2021-03-10 12:57:27 -0300

    Arganização e adicionado exemplo

---
+++ Assets/exemplo/scripts/MeuController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MeuController : BaseMGController
+{
+
+    protected override void EndGame()
+    {
+        Debug.Log("Não Implementada End");
+    }
+
+    protected override void WinGame()
+    {
+        Debug.Log("Não Implementada Win");
+    }
+
+    private void LateUpdate()
+    {
+        //if (GameData.level == 5) GameData.lost = true;
+    }
+
+    
+}
