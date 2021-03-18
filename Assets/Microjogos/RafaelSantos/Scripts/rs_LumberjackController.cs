25c4262f53cf3f03665ce47fbd4d498f98acbd2f
4rfel <38050598+4rfel@users.noreply.github.com>
2021-03-18 09:47:31 -0300

    random start

--- Assets/Microjogos/RafaelSantos/Scripts/rs_LumberjackController.cs
+++ Assets/Microjogos/RafaelSantos/Scripts/rs_LumberjackController.cs
@@ -8,6 +8,16 @@ public class rs_LumberjackController : MonoBehaviour
 
     public int lado = 0;
 
+    void Start()
+    {
+        float x = Random.Range(0f, 1f);
+        if (x <= 0.5)
+        {
+            transform.position = new Vector3(2.5f, -3.6f, 0f);
+            lado = 1;
+        }
+    }
+
     void Update()
     {
         if (controller.end || !controller.start) return;
