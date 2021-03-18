f0ce0dddf16b19d66e611127931c626518aac05f
fontes99 <pvfontes1000@gmail.com>
2021-03-18 16:06:00 -0300

    sprites & mechanics

---
+++ Assets/Microjogos/PedroFontes/_scripts/MGController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MGController : BaseMGController
+{
+
+    public GameObject faller;
+
+    protected override void EndMicrogame()
+    {
+        if(GameData.lost)
+        {
+            GameManager.Text.text = "PUTS!";
+        }
+        else
+        {
+            GameManager.Text.text = "Você ganhou!";
+        }
+        
+    }
+
+    protected override void StartMicrogame()
+    {
+        // Mensagem inicial
+        GameManager.Text.text = "Catch!";
+
+    }
+
+    protected override void Microgame()
+    {
+        
+
+    }
+
+    private void Update()
+    {
+
+
+    }
+    
+}
