ff899564095871836e61d0676359a10e1f98d916
gabrielztk <gabrielztk@al.insper.edu.br>
2021-03-20 16:47:51 -0300

    Jogo basico

---
+++ Assets/Microjogos/GabrielZanetti/scripts/AudioManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+   [SerializeField]
+   private AudioSource sfxSource = default;
+   [SerializeField]
+   private AudioSource ambienceSource = default;
+   [SerializeField]
+   private AudioClip music = default;
+   private static AudioManager _instance;
+}
---
+++ Assets/Microjogos/GabrielZanetti/scripts/ComportamentoArmaGabrielZanetti.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComportamentoArmaGabrielZanetti : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform cano;
+    public GameObject tiro;
+    void Start()
+    {
+        
+    }
+
+    public void Atira() 
+    {
+        Debug.Log("Não Implementada Atirar");
+        Instantiate(tiro, cano.transform.position, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
