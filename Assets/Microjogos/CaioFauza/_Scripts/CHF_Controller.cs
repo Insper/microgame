f4bd4fd5a33d23efc73a2b53bfa297e0322cf0ce
CaioFauza <caiofauza@gmail.com>
2021-03-21 13:09:58 -0300

    Food runner v1.0

---
+++ Assets/Microjogos/CaioFauza/_Scripts/CHF_Controller.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CHF_Controller : BaseMGController
+{
+    private Rigidbody2D body;
+    Text textLevel;
+    
+    protected override void StartMicrogame()
+    {
+        GameManager.Text.text = "Don't collide!";
+        body = GetComponent<Rigidbody2D>();
+        textLevel = GameObject.Find("Level").GetComponent<Text>();
+    }
+
+    protected override void Microgame()
+    {
+    }
+
+    protected override void EndMicrogame()
+    {
+        GameManager.Text.text =  GameData.lost ? "You lose!" : "You win!";
+    }
+
+    void FixedUpdate()
+    {
+        float yInput = Input.GetAxis("Vertical");
+        float xInput = Input.GetAxis("Horizontal");
+        Vector2 viewportPosition = Camera.main.WorldToViewportPoint(transform.position);
+        if(viewportPosition.x < 0 || viewportPosition.x > 1) xInput *= -1;
+        if(viewportPosition.y < 0 || viewportPosition.y > 1) yInput *= -1;
+
+        int speed = 10;
+        textLevel.text = $"Difficulty level: 1";
+
+        if(GameData.level > 3)
+        {
+            speed = 5;
+            textLevel.text = $"Difficulty level: 2";
+        } else if(GameData.level > 6)
+        {
+            speed = 2;
+            textLevel.text = $"Difficulty level: 3";
+        }
+
+        body.MovePosition(body.position + new Vector2(xInput*speed, yInput*speed) * Time.fixedDeltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Food"))
+        {   
+            GameData.lost = true;
+            EndMicrogame();
+        }
+    }
+}
