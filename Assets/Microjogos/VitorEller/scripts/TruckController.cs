13e9a3eb69745d92786cf99120265c2c66bae566
VFermat <vitor.eller30@gmail.com>
2021-03-22 10:47:13 -0300

    V1 do jogo

---
+++ Assets/Microjogos/VitorEller/scripts/TruckController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TruckController : SteerableBehaviour
+{
+
+    public GameObject shoot;
+    public Transform gun;
+    private bool shot = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float yInput = Input.GetAxis("Vertical");
+        Thrust(0, yInput);
+        if (Input.GetAxis("Jump") != 0) {
+                Shoot();
+        }
+    }
+
+    public void Shoot()
+    {
+        if (!shot) {
+            Instantiate(shoot, gun.position, Quaternion.identity);
+            shot = true;
+        }
+    }
+}
---
+++ Assets/Microjogos/VitorEller/scripts/VitorgeController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class VitorgeController : BaseMGController
+{
+    public GameObject target;
+    public GameObject truck;
+
+    protected override void EndMicrogame()
+    {
+        GameObject target = GameObject.FindGameObjectWithTag("VitorgeTarget");
+        if (target) {
+            GameData.lost = true;
+        } else {
+            Debug.Log("WON");
+        }
+        Debug.Log("Não Implementada End");
+    }
+
+    protected override void StartMicrogame()
+    {
+
+        Debug.Log("Não Implementada Start");
+    }
+
+    protected override void Microgame()
+    {
+        Debug.Log("Não Implementada");
+        GameManager.Text.text = GameData.GetTime().ToString();
+    }
+
+    private void LateUpdate()
+    {
+        if (GameData.level %2 == 0) GameData.lost = true;
+    }
+
+    
+}
