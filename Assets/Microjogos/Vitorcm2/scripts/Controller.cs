041ada0594171d722f6edba891e371d5b7bcb8ae
vitorcm2 <vitorcm2@al.insper.edu.br>
2021-03-22 12:31:03 -0300

    Jogo da Roleta

---
+++ Assets/Microjogos/Vitorcm2/scripts/Controller.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Controller : BaseMGController
+{
+
+    // Sprites a serem gerenciados
+
+    // Exemplo de finalização de jogo
+    protected override void EndMicrogame()
+    {
+        // Mensagens de vítoria ou derrota
+        if(GameData.lost)
+        {
+            GameManager.Text.text = "Você perdeu!";
+        }
+        else
+        {
+            GameManager.Text.text = "Você ganhou!";
+        }
+        GameManager.Text.color = Color.white;
+        
+    }
+
+    // Exemplo de inicialização de jogo
+    protected override void StartMicrogame()
+    {   
+
+        // Mensagem inicial
+        GameManager.Text.color = Color.black;
+        GameManager.Text.text = "Acerte o verde com a barra de espaço";
+
+    }
+
+    // Exemplo de jogo principal
+    protected override void Microgame()
+    {
+        GameData.lost = true;
+    }
+
+    
+}
