63ac90f6d3871505c4234e20a0621064acad8a8d
Henry Rocha <henryfr1@al.insper.edu.br>
2021-03-23 00:29:58 -0300

    Add docs on maze generator

--- Assets/Microjogos/HenryRocha/Scripts/hr_MazeGenerator.cs
+++ Assets/Microjogos/HenryRocha/Scripts/hr_MazeGenerator.cs
@@ -21,28 +21,38 @@ using System;
 [Flags]
 public enum WallState
 {
+    // A representação da parede é feita usando bits, por isso cada
+    // valor é uma potência de 2.
+    // Caso uma célula tenha todas as paredes e já tenha sido visitada
+    // esse será o WallState dela: 1000 1111
     LEFT = 1, // 0000 0001
-    RIGHT = 2,
-    UP = 4,
-    DOWN = 8,
-    VISITED = 128,
+    RIGHT = 2, // 0000 0010
+    UP = 4, // 0000 0100
+    DOWN = 8, // 0000 1000
+    VISITED = 128, // 1000 0000
 }
 
 public struct Position
 {
+    // Struct usado para guardar qual a posição atual no labirinto.
     public int X;
     public int Y;
 }
 
 public struct Neighbour
 {
+    // Struct que indica o vizinho de uma célula.
+
+    // Posição do visinho.
     public Position Position;
+
+    // Paredes compartilhadas com o vizinho.
     public WallState SharedWall;
 }
 
 public static class hr_MazeGenerator
 {
-
+    // Gets the opposite wall of a shared wall.
     private static WallState GetOppositeWall(WallState wall)
     {
         switch (wall)
@@ -55,33 +65,56 @@ public static class hr_MazeGenerator
         }
     }
 
+
     private static WallState[,] ApplyRecursiveBacktracker(WallState[,] maze, int width, int height)
     {
-        // here we make changes
+        // A random generator is needed to choose a random position.
+        // Generates the starting position.
+        // If a seed is given, it makes the mazes reproducible.
         var rng = new System.Random(/*seed*/);
+
+        // Stack of positions.
         var positionStack = new Stack<Position>();
+
+        // Create a random position.
         var position = new Position { X = rng.Next(0, width), Y = rng.Next(0, height) };
 
-        maze[position.X, position.Y] |= WallState.VISITED;
+        // Marks the starting position as visited.
+        maze[position.X, position.Y] |= WallState.VISITED; // The state will be: 1000 1111.
         positionStack.Push(position);
 
+        // While there are position on the stack...
         while (positionStack.Count > 0)
         {
+            // Get the last position added to the stack.
             var current = positionStack.Pop();
+
+            // Get all the neighbours of the current position.
             var neighbours = GetUnvisitedNeighbours(current, maze, width, height);
 
+            // If the cell has neighbours, it means that are still cells which have not
+            // been visited yet.
             if (neighbours.Count > 0)
             {
+                // Add the current position back to the stack.
                 positionStack.Push(current);
 
+                // Take a random neighbour of the current position.
                 var randIndex = rng.Next(0, neighbours.Count);
                 var randomNeighbour = neighbours[randIndex];
-
                 var nPosition = randomNeighbour.Position;
+
+                // Remove the shared wall between the current cell and it's neighbour.
                 maze[current.X, current.Y] &= ~randomNeighbour.SharedWall;
+
+                // Remove the shared wall on the neighbour.
+                // (The shared wall is opposite of the current cell's wall.)
                 maze[nPosition.X, nPosition.Y] &= ~GetOppositeWall(randomNeighbour.SharedWall);
+
+                // Marks this neighbour as visited.
                 maze[nPosition.X, nPosition.Y] |= WallState.VISITED;
 
+                // Add the neighbour to the stack.
                 positionStack.Push(nPosition);
             }
         }
@@ -89,14 +122,21 @@ public static class hr_MazeGenerator
         return maze;
     }
 
+    // Gera uma lista de vizinhos que ainda não foram visitados.
     private static List<Neighbour> GetUnvisitedNeighbours(Position p, WallState[,] maze, int width, int height)
     {
+        // List of neighbours.
         var list = new List<Neighbour>();
 
+        // Checks the left wall.
+        // This is needed to prevent deleting the left border of the maze.
         if (p.X > 0)
-        {
+        {   
+            // Verify if the cell to left has already been visited,
+            // if not, add it to the list of neighbours.
             if (!maze[p.X - 1, p.Y].HasFlag(WallState.VISITED))
             {
+                // Create a new neighbour, with the shared wall on the left.
                 list.Add(new Neighbour
                 {
                     Position = new Position
@@ -109,10 +149,15 @@ public static class hr_MazeGenerator
             }
         }
 
+        // Checks the bottom wall.
+        // This is needed to prevent deleting the bottom border of the maze.
         if (p.Y > 0)
         {
+            // Verify if the cell to the bottom has already been visited,
+            // if not, add it to the list of neighbours.
             if (!maze[p.X, p.Y - 1].HasFlag(WallState.VISITED))
             {
+                // Create a new neighbour, with the shared wall on the bottom.
                 list.Add(new Neighbour
                 {
                     Position = new Position
@@ -125,10 +170,15 @@ public static class hr_MazeGenerator
             }
         }
 
+        // Checks the top wall.
+        // This is needed to prevent deleting the top border of the maze.
         if (p.Y < height - 1)
         {
+            // Verify if the cell to the top has already been visited,
+            // if not, add it to the list of neighbours.
             if (!maze[p.X, p.Y + 1].HasFlag(WallState.VISITED))
             {
+                // Create a new neighbour, with the shared wall on the top.
                 list.Add(new Neighbour
                 {
                     Position = new Position
@@ -141,10 +191,15 @@ public static class hr_MazeGenerator
             }
         }
 
+        // Checks the right wall.
+        // This is needed to prevent deleting the right border of the maze.
         if (p.X < width - 1)
-        {
+        {   
+            // Verify if the cell to the right has already been visited,
+            // if not, add it to the list of neighbours.
             if (!maze[p.X + 1, p.Y].HasFlag(WallState.VISITED))
             {
+                // Create a new neighbour, with the shared wall on the right.
                 list.Add(new Neighbour
                 {
                     Position = new Position
@@ -160,11 +215,16 @@ public static class hr_MazeGenerator
         return list;
     }
 
+    // Create a 2D array of WallStates..
     public static WallState[,] Generate(int width, int height)
     {
+        // Create the maze variable, a 2D array of size width x height.
         WallState[,] maze = new WallState[width, height];
+
+        // The initial state of each cell of the maze.
         WallState initial = WallState.RIGHT | WallState.LEFT | WallState.UP | WallState.DOWN;
 
+        // Fill the maze with the initial state.
         for (int i = 0; i < width; ++i)
         {
             for (int j = 0; j < height; ++j)
