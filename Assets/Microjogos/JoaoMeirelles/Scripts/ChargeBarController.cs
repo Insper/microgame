ea982dc5856d9785b64c5c61e63afe3c2485f493
João Meirelles <joaopmjm@al.insper.edu.br>
2021-03-23 20:35:18 -0300

    Adding randomized target

--- Assets/Microjogos/JoaoMeirelles/Scripts/ChargeBarController.cs
+++ Assets/Microjogos/JoaoMeirelles/Scripts/ChargeBarController.cs
@@ -9,20 +9,24 @@ public class ChargeBarController : MonoBehaviour
     double chargeDelta = 10.0f;
     public GameObject Cannon;
     public GameObject CannonBall;
+    public GameObject Target;
+    GameObject chargeBar;
     float maxCharge = 30.0f;
     bool keyIsDown = false;
     bool launch = false;
     public float angle;
     int numberOfShots = 1;
     int shotsMade = 0;
-    GameObject chargeBar;
+    
     void Start()
     {
         chargeBar = GameObject.FindGameObjectsWithTag("ChargeBar")[0];
         charge = 0;
         angle = Random.Range(-20.0f,-75.0f);
         Debug.Log("Rotation " + angle);
-        transform.Rotate(0.0f,0.0f,angle);
+        //transform.Rotate(0.0f,0.0f,angle);
+        GameObject newObject = Instantiate(Target, new Vector3(Random.Range(-5.0f,8.0f),-3.75f,0.0f),Quaternion.identity) as GameObject;  // instatiate the object
+        newObject.transform.localScale = new Vector3(Random.Range(1.0f,3.0f), 0.5f, 1.0f);
         Instantiate(Cannon, transform.position, Quaternion.AngleAxis(angle, Vector3.forward));
     }
 
