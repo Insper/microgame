8ae2369cd39c1fe05393ba02d97bdb33ac31f6b1
Luiz Vitor <luvi@germanos.ws>
2021-03-24 01:50:34 -0300

    flapphy

---
+++ Assets/Microjogos/LuizTeixeira/scripts/ControllerLuizTeixeira.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+// Controller principal do jogo
+public class ControllerLuizTeixeira : BaseMGController
+{
+
+    // Objetos a serem gerenciados
+    public Rigidbody2D bird;
+    public float velocity;
+
+
+    // Exemplo de finalização de jogo
+    protected override void EndMicrogame()
+    {
+        // Mensagens de vítoria ou derrota
+        if (GameData.lost)
+        {
+            GameManager.Text.text = "Você perdeu!";
+        }
+        else
+        {
+            GameManager.Text.text = "Você ganhou!";
+        }
+
+    }
+
+    // Exemplo de inicialização de jogo
+    protected override void StartMicrogame()
+    {
+        // Mensagem inicial
+        GameManager.Text.text = "Não encoste nos tubos!";
+
+    }
+
+    // Exemplo de jogo principal
+    protected override void Microgame()
+    {
+
+    }
+
+    // Método chamado continuamente a cada quadro
+    private void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            bird.velocity = Vector2.up * velocity;
+        }
+        
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameData.lost = true;
+    }
+
+}
---
+++ Assets/Microjogos/LuizTeixeira/scripts/TubeSpawner.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TubeSpawner : MonoBehaviour
+{
+    public GameObject tube;
+    public float height;
+    public float maxTimer = 1;
+    public float timer = 1;
+    // Use this for initialization
+    void Start()
+    {
+        GameObject newTube = Instantiate(tube);
+        newTube.transform.position = transform.position + new Vector3(0, Random.Range(-height, height), 0);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer > maxTimer)
+        {
+            GameObject newTube = Instantiate(tube);
+            newTube.transform.position = transform.position + new Vector3(0, Random.Range(-height, height), 0);
+            Destroy(newTube, 20);
+            timer = 0;
+        }
+        timer += Time.deltaTime;
+    }
+}
