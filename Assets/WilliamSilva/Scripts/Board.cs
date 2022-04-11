e732227efb3e81bef7cd59f2545a774a462b94cf
William Silva <william.silva.ismart@gmail.com>
2022-04-11 10:59:11 -0300

    tutorial spawning

---
+++ Assets/WilliamSilva/Scripts/Board.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Board : MonoBehaviour
+{
+    public Tilemap tilemap { get; private set; }
+    public Piece activePiece { get; private set; }
+    public TetrominoData[] tetrominoes;
+    public Vector3Int spawnPosition;
+
+    private void Awake()
+    {
+        this.tilemap = GetComponentInChildren<Tilemap>();
+        this.activePiece = GetComponentInChildren<Piece>();
+
+        for(int i = 0; i < this.tetrominoes.Length; i++)
+        {
+            this.tetrominoes[i].Initialize();
+        }
+    }
+
+    private void Start()
+    {
+        SpawnPiece();
+    }
+
+    public void SpawnPiece()
+    {
+        int random = Random.Range(0, this.tetrominoes.Length);
+        TetrominoData data = this.tetrominoes[random];
+
+        this.activePiece.Initialize(this, this.spawnPosition, data);
+        Set(this.activePiece);
+    }
+
+    public void Set(Piece piece)
+    {
+        for(int i = 0; i < piece.cells.Length; i++)
+        {
+            Vector3Int position = piece.cells[i] + piece.position;
+            this.tilemap.SetTile(position, piece.data.tile);
+        }
+    }
+
+}
