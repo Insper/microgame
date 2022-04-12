f53ab7a6fee0e733ceb37c9784d8f98b66a7353a
William Silva <william.silva.ismart@gmail.com>
2022-04-11 22:47:02 -0300

    movement and rotation

--- Assets/WilliamSilva/Scripts/Board.cs
+++ Assets/WilliamSilva/Scripts/Board.cs
@@ -7,6 +7,16 @@ public class Board : MonoBehaviour
     public Piece activePiece { get; private set; }
     public TetrominoData[] tetrominoes;
     public Vector3Int spawnPosition;
+    public Vector2Int boardSize = new Vector2Int(18, 10);
+
+    public RectInt Bounds
+    {
+        get
+        {
+            Vector2Int position = new Vector2Int(-this.boardSize.x / 2, -this.boardSize.y / 2);
+            return new RectInt(position, this.boardSize);
+        }
+    }
 
     private void Awake()
     {
@@ -40,6 +50,34 @@ public class Board : MonoBehaviour
             Vector3Int position = piece.cells[i] + piece.position;
             this.tilemap.SetTile(position, piece.data.tile);
         }
+    }
+    
+    public void Clear(Piece piece)
+    {
+        for(int i = 0; i < piece.cells.Length; i++)
+        {
+            Vector3Int position = piece.cells[i] + piece.position;
+            this.tilemap.SetTile(position, null);
+        }
+    }
+
+    public bool IsValidPosition(Piece piece, Vector3Int position)
+    {
+        RectInt bounds = this.Bounds;
+
+        for(int i = 0; i < piece.cells.Length; i++){
+            Vector3Int tilePosition = piece.cells[i] + position;
+
+            if (!bounds.Contains( (Vector2Int) tilePosition)) {
+                return false;
+            }
+
+            if(this.tilemap.HasTile(tilePosition)){
+                return false;
+            }
+        }
+
+        return true;
     }
 
 }
