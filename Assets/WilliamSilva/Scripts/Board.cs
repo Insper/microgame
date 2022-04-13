9eaf69c9314be7df52811a5a0bbb5978ee099c70
William Silva <william.silva.ismart@gmail.com>
2022-04-13 00:08:46 -0300

    Game working very well

--- Assets/WilliamSilva/Scripts/Board.cs
+++ Assets/WilliamSilva/Scripts/Board.cs
@@ -40,7 +40,19 @@ public class Board : MonoBehaviour
         TetrominoData data = this.tetrominoes[random];
 
         this.activePiece.Initialize(this, this.spawnPosition, data);
-        Set(this.activePiece);
+
+        if (IsValidPosition(this.activePiece, this.spawnPosition)){
+            Set(this.activePiece);
+        } else {
+            GameOver();
+        }
+
+    }
+
+    private void GameOver()
+    {
+        this.tilemap.ClearAllTiles();
+        // Here I can do my things
     }
 
     public void Set(Piece piece)
@@ -78,6 +90,64 @@ public class Board : MonoBehaviour
         }
 
         return true;
+    }
+
+    public void ClearLines()
+    {
+        RectInt bounds = this.Bounds;
+        int row = bounds.yMin;
+
+        while (row < bounds.yMax)
+        {
+            if(IsLineFull(row)){
+                LineClear(row);
+            } else {
+                row++;
+            }
+        }
+
+
+    }
+
+    private bool IsLineFull(int row)
+    {
+        RectInt bounds = this.Bounds;
+
+        for(int col = bounds.xMin; col < bounds.xMax; col++)
+        {
+            Vector3Int position = new Vector3Int(col, row, 0);
+            if(!this.tilemap.HasTile(position))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private void LineClear(int row)
+    {
+        RectInt bounds = this.Bounds;
+        for(int col = bounds.xMin; col < bounds.xMax; col++)
+        {
+            Vector3Int position = new Vector3Int(col, row, 0);
+            this.tilemap.SetTile(position, null);
+        }
+
+        while (row < bounds.yMax)
+        {
+            for(int col = bounds.xMin; col < bounds.xMax; col++)
+            {
+                Vector3Int position = new Vector3Int(col, row + 1, 0);
+                TileBase above = this.tilemap.GetTile(position);
+                position = new Vector3Int(col, row, 0);
+
+                this.tilemap.SetTile(position, above);
+            }
+
+            row++;
+        }
+
     }
 
 }
--- Assets/WilliamSilva/Scripts/Piece.cs
+++ Assets/WilliamSilva/Scripts/Piece.cs
@@ -86,6 +86,7 @@ public class Piece : MonoBehaviour
     private void Lock()
     {
         this.board.Set(this);
+        this.board.ClearLines();
         this.board.SpawnPiece();
     }
 
