c150eb6d05e60d5eee3a876a18f16d8579af1299
lucaskf1996 <email@example.com>
2022-04-18 17:28:17 -0300

    feito

---
+++ Assets/Fuka/scripts/guloseimasSpawner.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Fuka {
+    public class guloseimasSpawner : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        public GameObject[] spots;
+        private Fuka.GameManager fukaGm;
+        void Start()
+        {
+            // fukaGm = Fuka.GameManager.GetInstance();
+            // for
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+}
