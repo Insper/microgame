4b0788f47bc1d6b8c20edd0b294ce58956f672c2
VergaraC <victorvaac@al.insper.edu.br>
2022-04-18 15:08:03 -0300

    .

---
+++ Assets/VictorVergara/EggMovement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Vergara{
+    public class EggMovement : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        private Rigidbody2D rb2D;
+
+            void Awake()
+            {
+                rb2D = gameObject.GetComponent<Rigidbody2D>();
+            }
+        
+            void FixedUpdate() {
+            
+                float inputX = Input.GetAxis("Horizontal");
+                float inputY = Input.GetAxis("Vertical");
+                transform.position += new Vector3(inputX, inputY, 0) * Time.deltaTime * 5f; 
+       
+            }
+    }
+}
