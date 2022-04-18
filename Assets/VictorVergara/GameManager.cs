956d43321c9f9d2a244888163043b52e7df5e36e
VergaraC <victorvaac@al.insper.edu.br>
2022-04-18 02:50:02 -0300

    l

--- Assets/VictorVergara/GameManager.cs
+++ Assets/VictorVergara/GameManager.cs
@@ -9,11 +9,25 @@ namespace Vergara{
         private MicrogameInternal.GameManager gm;
         private int[] _progression = {2, 3, 5, 4, 3, 2}; 
         private int _level;
+        public GameObject instructions;  // Textou das instruçõees
+        // Eggs
+        public GameObject Blue;  
+        public GameObject Green; 
+        public GameObject Orange;
+        public GameObject Purple;
+        public GameObject Red;
+        public GameObject Yellow;
+        // Dinos
+        public GameObject DinoBlue;
+        public GameObject DinoRed;
+        public GameObject DinoYellow;
+        public GameObject DinoGreen;
 
         // Start is called before the first frame update
         void Start(){
-            gm = MicrogameInternal.GameManager.GetInstance();    
+            gm = MicrogameInternal.GameManager.GetInstance();
             _level = gm.ActiveLevel <= 2 ? gm.ActiveLevel : 2;
+            Invoke(nameof(Begin), 0.5f);
             //spawna os dinos
             // spawna o ovo da vez        
         }
@@ -22,7 +36,38 @@ namespace Vergara{
         void Update(){
         }
         void Begin() {
+            instructions.SetActive(false);  // Tira tela de instruções
             Invoke(nameof(EndCheck), gm.MaxTime-0.1f);
+
+            // Cria objetos na cena
+            //for(int i=0; i<4; i++){
+                //par 1
+                Vector3 position = new Vector3(5.5f, 2.5f, 0f);
+                GameObject par11 = Instantiate(DinoBlue, position, Quaternion.identity, transform);
+                Vector3 position2 = new Vector3(6.3f, 2.5f, 0f);
+                GameObject par12 = Instantiate(DinoGreen, position2, Quaternion.identity, transform);
+
+                //par 2 aka par certo
+                Vector3 position3 = new Vector3(5.5f, -2.5f, 0f); //mudar
+                GameObject par21 = Instantiate(DinoRed, position3, Quaternion.identity, transform);
+                Vector3 position4 = new Vector3(6.3f, -2.5f, 0f); //mudar
+                GameObject par22 = Instantiate(DinoBlue, position4, Quaternion.identity, transform);
+
+                //par 3
+                Vector3 position5 = new Vector3(0, 2.5f, 0f); //mudar
+                GameObject par31 = Instantiate(DinoGreen, position5, Quaternion.identity, transform);
+                Vector3 position6 = new Vector3(0.8f, 2.5f, 0f); //mudar
+                GameObject par32 = Instantiate(DinoYellow, position6, Quaternion.identity, transform);
+                
+                //par 4
+                Vector3 position7 = new Vector3(0, -2.5f, 0f); //mudar
+                GameObject par41 = Instantiate(DinoYellow, position7, Quaternion.identity, transform);
+                Vector3 position8 = new Vector3(0.8f, -2.5f, 0f); //mudar
+                GameObject par42 = Instantiate(DinoRed, position8, Quaternion.identity, transform);
+           // }
+            Vector3 positionEgg = new Vector3(-5f, 0f, 0f); //mudar
+            GameObject egg = Instantiate(Purple, positionEgg, Quaternion.identity, transform);
+
             gm.StartTimer(); 
         }
 
